88f6a990515e0222b5bbdf6b9a056d4e006d62ac
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-01 00:44:32 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine( ));
+int count = 2;
+while (count<=value)
+if (count%2==0)
+{
+  System.Console.WriteLine(count); 
+  count=count+2;
+}
